90b3400797f216895614b642b64cb09612b1b609
Abdtabish <siddiquitabish321.com>
2025-12-16 12:57:50 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join("artifact","train.csv")
@@ -50,7 +53,9 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()    
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)        
+    train_array,test_array,_=data_transformation.initiate_data_transformation(train_data,test_data)        
                 
+    modlel_trainer=ModelTrainer()
+    print(modlel_trainer.initiate_model_trainer(train_array,test_array))           
 
 
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import dill
 
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 
 import numpy as np
@@ -18,3 +19,24 @@ def save_objects(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)        
+
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train)
+
+            train_predict=model.predict(X_train)
+            test_predict=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,train_predict)
+            test_model_score=r2_score(y_test,test_predict)
+
+            report[list(models.keys())[i]]=test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e,sys)    
